edc30c802f26ad23d96c8bb8d18d14ffc3c26144
Fossey-fr <188269976+Fossey-fr@users.noreply.github.com>
2025-10-24 07:13:46 -0400

    Add pseudo code

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// create a string variable for NPC choice
+
+// create a string variable for players choice
+
+// create an integer variable for rounds played
+
+// create an integer variable for rounds won
+
+
+
+
+
+// store a random RPS choice for NPC in corresponding variable 
+
+// ask user for RPS choice and store in corresponding variable
+
+// if user and NPC choices are identical, restart round
+
+// compare user and npc choices to determine round winner
+
+// create and/or increment rounds-won as an integer variable
+
+// increment games played variable
+
+// start a new round
+
+// announce winner after 5 rounds
+
+
+
+// QUESTIONS
+// Q. how do i want to handle round ties? A. restart round
+// Q. how do i want to handle match ties? A. match ties not possible
